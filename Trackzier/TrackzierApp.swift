00c2d84d608ecc0146355f9c635d14ff068c13fc
Luciano Silva <legostr@gmail.com>
2023-10-23 21:34:44 -0300

    Initial Commit

---
+++ Trackzier/TrackzierApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackzierApp.swift
+//  Trackzier
+//
+//  Created by Luciano Clécio da Silva on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackzierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
