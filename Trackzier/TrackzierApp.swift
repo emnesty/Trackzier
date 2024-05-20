5c6b7a85c74e4940f8e0dc266482d2da1d7f0afe
emnesty <legostr@gmail.com>
2024-05-19 22:36:03 -0300

    Initial Commit

---
+++ Trackzier/TrackzierApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackzierApp.swift
+//  Trackzier
+//
+//  Created by Luciano Silva on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackzierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
