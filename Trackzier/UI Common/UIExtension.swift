f39ed1c6567845ae87c2759fa17dae1625ebeed7
emnesty <legostr@gmail.com>
2024-05-21 10:55:13 -0300

    First Commit

---
+++ Trackzier/UI Common/UIExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtension.swift
+//  Trackzier
+//
+//  Created by Luciano Silva on 19/05/24.
+//
+
+import SwiftUI
+
+struct UIExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtension()
+}
---
+++ Trackzier/View/Login/Welcome.swift
@@ -0,0 +1,18 @@
+//
+//  Welcome.swift
+//  Trackzier
+//
+//  Created by Luciano Silva on 19/05/24.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome()
+}
