f6a390e72bc3670f04663413dbc9dc549afe5e68
emnesty <legostr@gmail.com>
2024-05-21 21:18:28 -0300

    New Buttons

---
+++ Trackzier/UI Common/PrimaryButton.swift
@@ -0,0 +1,18 @@
+//
+//  PrimaryButton.swift
+//  Trackzier
+//
+//  Created by Luciano Silva on 21/05/24.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrimaryButton()
+}
