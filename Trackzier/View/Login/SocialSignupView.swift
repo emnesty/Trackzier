74d42f5892da5e58b67f8e19ffbed4b06d8514ab
Luciano Clécio da Silva <legostr@gmail.com>
2024-05-22 15:11:08 -0300

    New Buttons and Views

---
+++ Trackzier/View/Login/SocialSignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SocialSignupView.swift
+//  Trackzier
+//
+//  Created by Luciano Clécio da Silva on 22/05/24.
+//
+
+import SwiftUI
+
+struct SocialSignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SocialSignupView()
+}
